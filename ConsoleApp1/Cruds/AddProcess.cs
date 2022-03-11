7ab8547eeac11950e867b1d582289e428cc532e3
Mr.Sadness <40417513+MrFrostman@users.noreply.github.com>
2022-03-10 21:26:47 -0700

    feat:10 min for developement

---
+++ ConsoleApp1/Cruds/AddProcess.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.List
+{
+    public class AddProcess
+    {
+    
+        public static void AgregarALista()
+        {
+            List<Process> ListaDeProcesos = new List<Process>();
+            ListaDeProcesos.Add(new Process() {Name, AproxTime});
+        }
+
+
+
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp1.Data;
+int Response1;
+string ListName
+
+
+
+//asignate the  value of process name
+
+Console.WriteLine("what you do it? 1.- make a process or 2 delete a process or 3 make a new list ");  Response1 = Console.Read();
+if (Response1 ==1)
+{
+    Process NewListProcess = new Process();
+    Console.WriteLine("Insert the name of the Process");
+    NewListProcess.Name=Console.ReadLine();
+    Console.WriteLine("Inser the Proximity time for the process");
+    NewListProcess.AproxTime=Console.Read();
+   
+}
+else if (Response1 == 2)
+{
+    Console.WriteLine("Inserte el nombre de la Lista");
+    ListName = Console.ReadLine();
+}
+else if(Response1 == 3)
+{
+    Console.WriteLine("Inserte el nombre de la Lista");
+    ListName=Console.ReadLine();
+
+}
+
